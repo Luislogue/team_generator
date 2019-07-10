d4f1819ed4d0407f02c895e98332ad2a5b407a6d
Luislogue <lulogue97@gmail.com>
2019-07-10 16:24:26 +0200

    Version 0.1 creada, estructura y funcionalidad basica finalizadad solo falta resolver error no pilla objetos en switch

--- models/Developers.php
+++ models/Developers.php
@@ -1,13 +1,18 @@
 <?php
 
  class Developer{
-
+    public $nombre
     public $rol;
     public $rol2;
 
-    public function __construct($rol,$rol2){
+    public function __construct($nombre,$rol,$rol2){
+
+        $this->nombre=$nombre;
         $this->rol=$rol;
         $this->rol2=$rol2;
+        
+    }
+    public function nombre($nombre){
         
     }
 
--- php/desarrolladores.php
+++ php/desarrolladores.php
@@ -1,13 +1,13 @@
 <?php
 require_once('models/Developers.php');
 
-$Alexis = new Developer('php','java');
-$David = new Developer('php','java');
-$Llordi = new Developer('HTML5/CSS3', 'Sass/Less');
-$Luis = new Developer('PYTHON', 'HTML5/CSS3');
-$Toni = new Developer('javascript', 'Laravel');
-$Fran = new Developer('PHP', 'Json/XML');
-$Julia = new Developer('Laravel', 'Sass/Less');
-$Lolo = new Developer('Java', 'Python');
-$Petro = new Developer('HTML5/CSS3', 'Javascript');
+$Alexis = new Developer('Alexis','php','java');
+$David = new Developer('David','php','java');
+$Llordi = new Developer('Llordi','HTML5/CSS3', 'Sass/Less');
+$Luis = new Developer('Luis','PYTHON', 'HTML5/CSS3');
+$Toni = new Developer('Toni','javascript', 'Laravel');
+$Fran = new Developer('Fran','PHP', 'Json/XML');
+$Julia = new Developer('Julia','Laravel', 'Sass/Less');
+$Lolo = new Developer('Lolo','Java', 'Python');
+$Petro = new Developer('Petro','HTML5/CSS3', 'Javascript');
 
